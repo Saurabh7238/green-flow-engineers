9285418248684d994f6c037256568ca55900cec1
Saurabh <saurabh705294@gmail.com>
2026-07-09 10:37:12 +0530

    chore: allow dev origin for HMR (allowedDevOrigins)

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	// Allow the local network host for HMR/dev resources when developing
+	allowedDevOrigins: ["10.138.114.154"],
+};
 
 export default withNextIntl(nextConfig);
